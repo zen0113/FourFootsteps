7c07da2f43de75c37f5e2b67b7d0a77a13e6d49d
Zen <z_zen@naver.com>
2025-06-01 21:49:05 +0900

    feat: 타이틀 버튼 연결

---
+++ Assets/02.Scripts/Title/TitleButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

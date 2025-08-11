ec66ec906b5ae3927c17a82798f958ba20c6adfc
viabe <skqldhkd123@naver.com>
2025-08-11 16:35:41 +0900

    feat : 나무 판자 생성

--- Assets/02.Scripts/InteractionObject/PushableBox.cs
+++ Assets/02.Scripts/InteractionObject/PushableBox.cs
@@ -9,8 +9,8 @@ public class PushableBox : MonoBehaviour
     private float originalMass;
     
     [Header("박스 물리 설정")]
-    [SerializeField] private float normalDrag = 10f;      // 일반 상태 drag
-    [SerializeField] private float interactingDrag = 1f;  // 상호작용 중 drag
+    [SerializeField] private float normalDrag = 2f;      // 일반 상태 drag
+    [SerializeField] private float interactingDrag = 0.5f;  // 상호작용 중 drag
     [SerializeField] private float boxMass = 2f;          // 박스 질량
     
     private void Start()

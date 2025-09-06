cc64e6ef5e66e5c8d38ac0e73ab9a171a8663085
Zen <z_zen@naver.com>
2025-09-07 01:03:54 +0900

    fix: 회상씬1 고양이 이동 추가

--- Assets/02.Scripts/Tutorial/Guide/HintAndInstallTutorial.cs
+++ Assets/02.Scripts/Tutorial/Guide/HintAndInstallTutorial.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HintAndInstallTutorial : TutorialBase
@@ -6,6 +7,7 @@ public class HintAndInstallTutorial : TutorialBase
     [Header("설치 관련")]
     [SerializeField] private GameObject hintOutline;
     [SerializeField] private GameObject installerPrefab;
+    [SerializeField] private List<GameObject> activateObjects = new List<GameObject>();
     [SerializeField] private GameObject hideObject;
 
     [Header("상호작용 UI")]
@@ -23,6 +25,16 @@ public class HintAndInstallTutorial : TutorialBase
         }
 
         hintOutline.SetActive(true);
+
+        // 리스트의 모든 오브젝트 활성화 (아웃라인과 같은 타이밍)
+        foreach (GameObject obj in activateObjects)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(true);
+            }
+        }
+
         isInstalled = false;
         isInteracting = false;
     }
@@ -48,19 +60,15 @@ public class HintAndInstallTutorial : TutorialBase
     private void OnPlayerInteract()
     {
         isInteracting = true;
-
         // 설치 처리
         hintOutline.SetActive(false);
         if (installerPrefab != null)
         {
             installerPrefab.SetActive(true);
         }
-
         isInstalled = true;
-
         // UI 숨기기
         interactionImageDisplay.ForceHideImage();
-
         // 다음 튜토리얼로 즉시 이동
         if (tutorialController != null)
         {
